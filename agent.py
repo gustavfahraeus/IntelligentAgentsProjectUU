2d32642e66538198029dc666a50dce2b28c86436
Gustav Fåhraeus <g.fahraeus@hotmail.com>
2021-10-19 16:30:13 +0200

    First Commit

---
+++ agent.py
@@ -0,0 +1,16 @@
+from owlready2 import *
+from pprint import pprint
+owlready2.JAVA_EXE = "C:/Users/gfahr/Documents/Protege-5.5.0/jre/bin/java.exe" # Change this to whatever on your own system. Don't know how to work around this yet.
+
+# We are developing a utility based Agent.
+class Agent:
+
+    def __init__(self):
+        # Load the desired ontology using the path file
+        self.ontology = get_ontology('./group11.owl').load()
+
+        with self.ontology:
+            sync_reasoner()
+
+
+
---
+++ run_script.py
@@ -0,0 +1,8 @@
+from agent import Agent
+
+def main():
+    agent = Agent()
+
+
+if __name__ == "__main__":
+    main()
