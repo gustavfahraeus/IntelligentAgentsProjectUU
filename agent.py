7f61e5df2b3c97d2f4da9cfa399d92af2c4fbd6d
Tessel11 <xelfjex@gmail.com>
2021-10-25 23:17:09 +0200

    adding scenario 1

--- agent.py
+++ agent.py
@@ -1,6 +1,6 @@
 from owlready2 import *
 from pprint import pprint
-owlready2.JAVA_EXE = "C:/Users/gfahr/Documents/Protege-5.5.0/jre/bin/java.exe" # Change this to whatever on your own system. Don't know how to work around this yet.
+owlready2.JAVA_EXE = "C:/Users/xelfj/Downloads/Protege-5.5.0-win/Protege-5.5.0/jre/bin/java.exe" # Change this to whatever on your own system. Don't know how to work around this yet.
 
 # We are developing a utility based Agent.
 class Agent:
@@ -11,6 +11,9 @@ class Agent:
 
         with self.ontology:
             sync_reasoner()
+
+    def get_restaurants(self):
+        return self.ontology.search(type = self.ontology.Restaurant)
 
 
 
--- run_script.py
+++ run_script.py
@@ -1,8 +1,25 @@
 from agent import Agent
 
+def get_scenario(i):
+    scenario = {}
+
+    filename = "scenarios/scenario{}.txt".format(i)
+    with open(filename) as f:
+        dic = ""
+        for line in f:
+            if "#" in line:
+                dic = line.strip()
+                scenario[dic] = {}
+            else:
+                (key, value) = line.split(" ")
+                scenario[dic][key] = value.strip()
+    
+    return scenario
+
 def main():
     agent = Agent()
-
+    scenario = get_scenario(1)
+    print(scenario)
 
 if __name__ == "__main__":
     main()
