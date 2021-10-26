5e60a031503e56f4250d927f80d594755ecbdba2
Gustav Fåhraeus <g.fahraeus@hotmail.com>
2021-10-26 16:18:34 +0200

    Working on the cuisine error in the ontology, also adding more restaurants

--- agent.py
+++ agent.py
@@ -1,6 +1,6 @@
 from owlready2 import *
 from pprint import pprint
-owlready2.JAVA_EXE = "C:/Users/gfahr/Documents/Protege-5.5.0/jre/bin/java.exe" # Change this to whatever on your own system. Don't know how to work around this yet.
+owlready2.JAVA_EXE = "c:/users/gfahr/onedrive/Dokument/Protege-5.5.0/jre/bin/java.exe" # Change this to whatever on your own system. Don't know how to work around this yet.
 
 # We are developing a utility based Agent.
 class Agent:
@@ -8,6 +8,7 @@ class Agent:
     def __init__(self):
         # Load the desired ontology using the path file
         self.ontology = get_ontology('./group11.owl').load()
+        print(self.ontology.search(iri = '*Constraint'))
 
         with self.ontology:
             sync_reasoner()
