84cf44af6bf9be2777f3bfe1db8c9c8d4d1a1b93
Tessel11 <xelfjex@gmail.com>
2021-10-28 14:30:30 +0200

    Added movie genre and duration + bad/good reasons

--- agent.py
+++ agent.py
@@ -13,12 +13,13 @@ class Agent:
             sync_reasoner()
 
     def get_restaurants(self, sc):
-        options = {"scores" : {}, "reasons": {}}
+        options = {"scores" : {}, "good_reasons": {}, "bad_reasons": {}}
 
         restaurants = self.ontology.search(type = self.ontology.Restaurant)
         for res in restaurants:
             utility = 1
-            reasons = []
+            good_reasons = []
+            bad_reasons = []
 
             #Check Cuisines
             for key, value in sc["restaurant_pref"]["cuisine"].items():
@@ -27,7 +28,9 @@ class Agent:
                     if res in list(resI):
                         new_utility = value * utility
                         if new_utility > utility:
-                            reasons.append(" - This restaurant has an Italian Cuisine")
+                            good_reasons.append(" - This restaurant has an Italian Cuisine")
+                        if new_utility < utility:
+                            bad_reasons.append(" - This restaurant has an Italian Cuisine")
                         utility = new_utility
 
                 if key == "FrenchCuisine":
@@ -35,7 +38,9 @@ class Agent:
                     if res in list(resF):
                         new_utility *= value                        
                         if new_utility > utility:
-                            reasons.append(" - This restaurant has an French Cuisine")
+                            good_reasons.append(" - This restaurant has an French Cuisine")
+                        if new_utility < utility:
+                            bad_reasons.append(" - This restaurant has an French Cuisine")
                         utility = new_utility
 
                 if key == "TurkishCuisine":
@@ -43,21 +48,88 @@ class Agent:
                     if res in list(resT):
                         new_utility *= value                        
                         if new_utility > utility:
-                            reasons.append(" - This restaurant has an Turkish Cuisine")
+                            good_reasons.append(" - This restaurant has an Turkish Cuisine")
+                        if new_utility < utility:
+                            good_reasons.append(" - This restaurant has an Turkish Cuisine")
                         utility = new_utility
             
 
-            #Check allergy:
-            users = self.ontology.search(user = "*")
-            user = [u for u in users if u.name[0] == sc["user"]][0]
-            self.ontology.search(hasAllergy = "*")
+            #Check allergy
+            #users = self.ontology.search(user = "*")
+            #user = [u for u in users if u.name[0] == sc["user"]][0]
+            #print(self.ontology.search(hasAllergy = "*"))
+
+            #Check nutrients
+
+            #Check pricerange
 
             if utility != 0:
                 restaurant = res.restaurantName[0]
                 options["scores"][restaurant] = utility
-                options["reasons"][restaurant] = reasons
+                options["good_reasons"][restaurant] = good_reasons
+                options["bad_reasons"][restaurant] = bad_reasons
         
         return options
+
+    def get_movies(self, sc):
+        options = {"scores" : {}, "good_reasons": {}, "bad_reasons": {}}
+
+        #Get movies that are at the right time
+        movies = self.ontology.search(type = self.ontology.Movie)
+
+        for movie in movies:
+            utility = 1
+            good_reasons = []
+            bad_reasons = []
+
+            #Check genre
+            for genre, value in sc["movie_pref"]["genre"]:
+                if movie.genre[0] == genre:
+                    new_utility = utility * value
+                    if new_utility > utility:
+                        good_reasons.append(" - This movie is a {}".format(genre))
+                    if new_utility < utility:
+                        bad_reasons.append(" - This movie is a {}".format(genre))
+                    utility = new_utility
+
+            #Check for duration
+            duration = movie.duration[0]
+            if sc["movie_pref"]["duration"] == "short":
+                if duration <= 90:
+                    utility *= 1.7
+                    good_reasons.append(" - The duration of this movie is {}, which is a short movie".format(movie.duration[0]))
+                if duration > 120:
+                    utility *= 0.4
+                    bad_reasons.append(" - The duration of this movie is {}, which is a long movie".format(movie.duration[0]))
+            if sc["movie_pref"]["duration"] == "average":
+                if duration > 90 & duration <= 120:
+                    utility *= 1.7
+                    good_reasons.append(" - The duration of this movie is {}, which is a average movie".format(movie.duration[0]))
+            if sc["movie_pref"]["duration"] == "long":
+                if duration <= 90:
+                    utility *= 0.4
+                    bad_reasons.append(" - The duration of this movie is {}, which is a short movie".format(movie.duration[0]))
+                if duration > 120:
+                    utility *= 1.7
+                    good_reasons.append(" - The duration of this movie is {}, which is a long movie".format(movie.duration[0]))
+            
+
+
+            #Adding movie to the options
+            if utility != 0:
+                movie_name = movie.nameMovie[0]
+                options["scores"][movie_name] = utility
+                options["good_reasons"][movie_name] = good_reasons
+                options["bad_reasons"][movie_name] = bad_reasons
+
+
+
+
+            
+
+
+
+
 
 
 
--- run_script.py
+++ run_script.py
@@ -8,22 +8,26 @@ def main():
     actions = {}
 
     if scenario["actions"]["cinema"] == 1:
-        actions["movie"] = agent.get_movies()
+        actions["movie"] = agent.get_movies(scenario)
 
     if scenario["actions"]["restaurant"] == 1:
         actions["restaurant"] = agent.get_restaurants(scenario)
 
     if scenario["actions"]["foodstore"] == 1:
-        actions["food store"] = agent.get_foodstores()
+        actions["food store"] = agent.get_foodstores(scenario)
 
     for action, options in actions.items():
         scores = options["scores"]
+        #Change to top 3!
         max_value = max(scores.values())
         good_options = [option for option, score in scores.items() if score == max_value]
         
         for suggestion in good_options:
             print("A good {} is {}, because:".format(action, suggestion))
-            print(*options["reasons"][suggestion], sep = "\n")
+            print(*options["good_reasons"][suggestion], sep = "\n")
+            if options["bad_reasons"][suggestion]:
+                print("But it is not a perfect option, because:")
+                print(*options["bad_reasons"][suggestion], sep = "\n")
 
 if __name__ == "__main__":
     main()
